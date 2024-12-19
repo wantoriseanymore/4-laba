6e78f3cd85664120ee02ae9bfdffb42414ba1011
wantoriseanymore <mihailbrun247@gmail.com>
2024-12-19 10:08:03 +0300

    Add files via upload

---
+++ 3 задание.py
@@ -0,0 +1,15 @@
+class Employee:
+    name = None
+    age = None
+    salary = None
+    pass
+
+people = Employee()
+
+people.name = "Nebledniy"
+people.age = 17
+people.salary = "12699"
+
+print(people.name)
+print(people.age)
+print(people.salary)
