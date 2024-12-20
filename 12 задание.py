dfea020a4dc01591fa86e6c6ae26dcf59c2a1449
wantoriseanymore <mihailbrun247@gmail.com>
2024-12-20 15:36:20 +0300

    Add files via upload

---
+++ 12 задание.py
@@ -0,0 +1,28 @@
+class User:
+    
+    def __init__(self,name,salary):
+        self.name = name
+        self.salary =salary
+	
+    def show(self):
+        return self.name
+        
+    def sal(self):
+        return self.salary
+   
+    def proc(self):
+        return self.salary + (self.salary / 100)*10
+   
+user = User('Romanov', 300)
+
+user.show()
+user.sal()
+user.proc()
+print(user.show())
+print(user.sal())
+print(user.proc())
+
+    
+    
+    
+    
